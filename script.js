cad6da3bc28180dd83fb09ea0109852140a6079e
Anid Camdzic <anidcamdzic@hotmail.com>
2021-03-19 01:24:56 +0100

    a

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ var app = angular.module("app", [])
 
 app.controller("mainController", function ($scope) {
     $scope.heroes = [
-        { picture: "https://i.pinimg.com/originals/19/b8/d6/19b8d6e9b13eef23ec9c746968bb88b1.jpg", name: "Merdan", description: "Super i smiren momak" },
+        { picture: "https://i.pinimg.com/originals/19/b8/d6/19b8d6e9b13eef23ec9c746968bb88b1.jpg", name: "Merdan", description: "Stalozen momak" },
         { picture: "https://i.pinimg.com/originals/19/b8/d6/19b8d6e9b13eef23ec9c746968bb88b1.jpg", name: "Alen", description: "100 kila gorila" },
         { picture: "https://i.pinimg.com/originals/19/b8/d6/19b8d6e9b13eef23ec9c746968bb88b1.jpg", name: "Ivana", description: "Los programer" },
         { picture: "https://i.pinimg.com/originals/19/b8/d6/19b8d6e9b13eef23ec9c746968bb88b1.jpg", name: "Jurica", description: "Isto ko ivana" },
-        { picture: "https://i.pinimg.com/originals/19/b8/d6/19b8d6e9b13eef23ec9c746968bb88b1.jpg", name: "Anid", description: "Blago njemu moze on" },
-        { picture: "https://i.pinimg.com/originals/19/b8/d6/19b8d6e9b13eef23ec9c746968bb88b1.jpg", name: "Qky", description: "Admin" }
+        { picture: "https://i.pinimg.com/originals/19/b8/d6/19b8d6e9b13eef23ec9c746968bb88b1.jpg", name: "Anid", description: "Alt + Z, Alt + Shift" },
+        { picture: "https://i.pinimg.com/originals/19/b8/d6/19b8d6e9b13eef23ec9c746968bb88b1.jpg", name: "Cookie", description: "Admin" }
     ]
 
     $scope.deleteHero = function (lik) {
